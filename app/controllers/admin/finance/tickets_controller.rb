7373d3e243e8257e1b329ea3eeec9a6d0b8005a9
hungmi <hungmihungmi@gmail.com>
2017-07-05 17:52:32 +0800

    disable #uploadBtn after upload

--- app/controllers/admin/finance/tickets_controller.rb
+++ app/controllers/admin/finance/tickets_controller.rb
@@ -4,12 +4,9 @@ class Admin::Finance::TicketsController < AdminController
 
 	def transform
 		file = params[:tickets_xml_file]
-		new_file_name = "#{file.original_filename.gsub('.xml', '')}_tickets.xls"
+		new_file_name = "#{file.original_filename.encode("gbk").gsub('.xml', '')}_tickets.xls"
 		new_file = DeliveryTicketService.transform file
-		# new_file.serialize "tmp/#{new_file_name}"
 		new_file.write "tmp/#{new_file_name}"
 		send_file "#{Rails.root}/tmp/#{new_file_name}", filename: new_file_name, type: "application/xls"
-		# flash[:success] = "轉換成功。"
-		# redirect_to admin_finance_tickets_uploading_url
 	end
 end
