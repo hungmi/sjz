82c9f541cc7213a2b30f60d47a8020aeba2ae0f4
hungmi <hungmihungmi@gmail.com>
2017-08-17 14:28:26 +0800

    disable pdf preview of docs; add image preview for docs#new, items#new

--- app/assets/javascripts/admin/helper.js
+++ app/assets/javascripts/admin/helper.js
@@ -1,21 +1,10 @@
-document.addEventListener("turbolinks:load", function(){
-	if ($("body.admin.items").length > 0) {
-		function showImage(src,target) {
-		  var fr=new FileReader();
-		  // when image is loaded, set the src of the image where you want to display it
-		  fr.onload = function(e) {
-		    target.src = this.result;
-		  };
-		  if (src != undefined && (src.length > 0)) {
-		  	src.addEventListener("change",function() {
-			    // fill fr with image data
-			    fr.readAsDataURL(src.files[0]);
-			  });	
-		  }
-		}
-
-		var src = document.getElementById("item_image");
-		var target = document.getElementById("target");
-		showImage(src,target);
-	}
+$(document).on("change", ".image_previewable", function(e){
+	var fr=new FileReader();
+	var target = document.getElementById("image_preview_target");
+	var src = e.target
+  // when image is loaded, set the src of the image where you want to display it
+  fr.onload = function(e) {
+    target.src = e.target.result;
+  };
+  fr.readAsDataURL(src.files[0]);
 })
